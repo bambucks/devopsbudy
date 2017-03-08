b53e1292829b7c8056fcd3496bc5b3ad1e4d5064
Bambang Hermanto <bambang@erpz.com>
2017-03-08 13:40:14 -0800

    Added index placeholder

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,16 +1,16 @@
-package com.devopsbuddy.controllers;
+package com.devopsbuddy.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by bhermanto on 2/14/2017.
+ * Created by bhermanto on 3/8/2017.
  */
 @Controller
-public class HelloWorldController {
+public class IndexController {
 
     @RequestMapping("/")
-    public String sayHello() {
+    public String home() {
         return "index";
     }
 }
