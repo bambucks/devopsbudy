6d9918046b9a20d923c7300fef682c9667f1ad28
Bambang Hermanto <bambang@erpz.com>
2017-03-09 11:35:20 -0800

    Adding internationalization

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,28 @@
 package com.devopsbuddy;
 
+import com.devopsbuddy.web.i18n.I18nService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Service
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18nService i18nService;
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18nService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
 	}
 
 }
